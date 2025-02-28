5f8f29a6dffe67e2897e9cf597cd430b23842e74
JesusPeraltaH <yessusyt@gmail.com>
2025-02-27 22:47:28 -0700

    a3

--- lib/inventory/sales.dart
+++ lib/inventory/sales.dart
@@ -8,7 +8,7 @@ class SalesPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Ventas'),
+        title: Text('Finanzas de Negocios'),
       ),
       body: StreamBuilder<List<Map<String, dynamic>>>(
         stream: _getBusinesses(),
@@ -30,24 +30,54 @@ class SalesPage extends StatelessWidget {
             itemCount: businesses.length,
             itemBuilder: (context, index) {
               var business = businesses[index];
-              return Card(
-                margin: EdgeInsets.all(8.0),
-                child: Padding(
-                  padding: EdgeInsets.all(16.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        business['nombreEmpresa'],
-                        style: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.bold),
-                      ),
-                      SizedBox(height: 8.0),
-                      // Aquí puedes agregar la lógica para obtener productos vendidos y total de ventas
-                      Text('Productos Vendidos: 0'), // Cambiar a la lógica real
-                      Text(
-                          'Total de Ventas: \$0.00'), // Cambiar a la lógica real
-                    ],
+              return GestureDetector(
+                onTap: () {
+                  _showBusinessFinancialDetails(context, business);
+                },
+                child: Card(
+                  margin: EdgeInsets.all(8.0),
+                  child: Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          business['nombreEmpresa'],
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(height: 8.0),
+                        FutureBuilder<Map<String, dynamic>>(
+                          future: _getFinancialData(business['id']),
+                          builder: (context, snapshot) {
+                            if (snapshot.connectionState ==
+                                ConnectionState.waiting) {
+                              return CircularProgressIndicator();
+                            }
+                            if (snapshot.hasError) {
+                              return Text('Error: ${snapshot.error}');
+                            }
+                            if (!snapshot.hasData) {
+                              return Text('No hay datos financieros');
+                            }
+
+                            var financialData = snapshot.data!;
+                            return Column(
+                              children: [
+                                Text(
+                                    'Ventas: \$${financialData['ventas'] ?? '0.00'}'),
+                                Text(
+                                    'Gastos: \$${financialData['gastos'] ?? '0.00'}'),
+                                Text(
+                                    'Inversiones: \$${financialData['inversiones'] ?? '0.00'}'),
+                                Text(
+                                    'Capital: \$${financialData['capital'] ?? '0.00'}'),
+                              ],
+                            );
+                          },
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               );
@@ -69,5 +99,233 @@ class SalesPage extends StatelessWidget {
                   'nombreEmpresa': doc['nombreEmpresa'],
                 })
             .toList());
+  }
+
+  // Obtener datos financieros de un negocio
+  Future<Map<String, dynamic>> _getFinancialData(String businessId) async {
+    // Obtener ventas
+    var salesSnapshot = await FirebaseFirestore.instance
+        .collection('sales')
+        .where('negocioId', isEqualTo: businessId)
+        .get();
+    double totalSales = salesSnapshot.docs.fold(0.0, (sum, doc) {
+      return sum + (doc['total'] as num).toDouble();
+    });
+
+    // Obtener gastos
+    var expensesSnapshot = await FirebaseFirestore.instance
+        .collection('gastos')
+        .where('negocioId', isEqualTo: businessId)
+        .get();
+    double totalExpenses = expensesSnapshot.docs.fold(0.0, (sum, doc) {
+      return sum + (doc['monto'] as num).toDouble();
+    });
+
+    // Obtener inversiones
+    var investmentsSnapshot = await FirebaseFirestore.instance
+        .collection('inversiones')
+        .where('negocioId', isEqualTo: businessId)
+        .get();
+    double totalInvestments = investmentsSnapshot.docs.fold(0.0, (sum, doc) {
+      return sum + (doc['monto'] as num).toDouble();
+    });
+
+    // Calcular capital total
+    double capital = totalSales - totalExpenses + totalInvestments;
+
+    return {
+      'ventas': totalSales.toStringAsFixed(2),
+      'gastos': totalExpenses.toStringAsFixed(2),
+      'inversiones': totalInvestments.toStringAsFixed(2),
+      'capital': capital.toStringAsFixed(2),
+    };
+  }
+
+  // Mostrar detalles financieros en un BottomSheet
+  void _showBusinessFinancialDetails(
+      BuildContext context, Map<String, dynamic> business) {
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      builder: (context) {
+        return FutureBuilder<Map<String, dynamic>>(
+          future: _getFinancialData(business['id']),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            }
+            if (snapshot.hasError) {
+              return Center(child: Text('Error: ${snapshot.error}'));
+            }
+            if (!snapshot.hasData) {
+              return Center(child: Text('No hay datos financieros'));
+            }
+
+            var financialData = snapshot.data!;
+            return SingleChildScrollView(
+              padding: EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    business['nombreEmpresa'],
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 16.0),
+                  _buildFinancialDetailItem(
+                      'Ventas Totales', financialData['ventas']),
+                  _buildFinancialDetailItem(
+                      'Gastos Totales', financialData['gastos']),
+                  _buildFinancialDetailItem(
+                      'Inversiones Totales', financialData['inversiones']),
+                  _buildFinancialDetailItem(
+                      'Capital Total', financialData['capital']),
+                  SizedBox(height: 16.0),
+                  ElevatedButton(
+                    onPressed: () {
+                      _addExpense(context, business['id']);
+                    },
+                    child: Text('Agregar Gasto'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      _addInvestment(context, business['id']);
+                    },
+                    child: Text('Agregar Inversión'),
+                  ),
+                ],
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+
+  // Widget para mostrar un detalle financiero
+  Widget _buildFinancialDetailItem(String title, String value) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            title,
+            style: TextStyle(fontSize: 16),
+          ),
+          Text(
+            '\$$value',
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
+    );
+  }
+
+  // Función para agregar un gasto
+  void _addExpense(BuildContext context, String businessId) {
+    TextEditingController nameController = TextEditingController();
+    TextEditingController amountController = TextEditingController();
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Agregar Gasto'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: nameController,
+                decoration: InputDecoration(labelText: 'Nombre del Gasto'),
+              ),
+              TextField(
+                controller: amountController,
+                decoration: InputDecoration(labelText: 'Monto'),
+                keyboardType: TextInputType.number,
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Cancelar'),
+            ),
+            TextButton(
+              onPressed: () async {
+                if (nameController.text.isNotEmpty &&
+                    amountController.text.isNotEmpty) {
+                  await FirebaseFirestore.instance.collection('gastos').add({
+                    'negocioId': businessId,
+                    'nombre': nameController.text,
+                    'monto': double.parse(amountController.text),
+                    'fecha': DateTime.now().toString(),
+                  });
+                  Navigator.of(context).pop();
+                }
+              },
+              child: Text('Guardar'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  // Función para agregar una inversión
+  void _addInvestment(BuildContext context, String businessId) {
+    TextEditingController nameController = TextEditingController();
+    TextEditingController amountController = TextEditingController();
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Agregar Inversión'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: nameController,
+                decoration:
+                    InputDecoration(labelText: 'Nombre de la Inversión'),
+              ),
+              TextField(
+                controller: amountController,
+                decoration: InputDecoration(labelText: 'Monto'),
+                keyboardType: TextInputType.number,
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Cancelar'),
+            ),
+            TextButton(
+              onPressed: () async {
+                if (nameController.text.isNotEmpty &&
+                    amountController.text.isNotEmpty) {
+                  await FirebaseFirestore.instance
+                      .collection('inversiones')
+                      .add({
+                    'negocioId': businessId,
+                    'nombre': nameController.text,
+                    'monto': double.parse(amountController.text),
+                    'fecha': DateTime.now().toString(),
+                  });
+                  Navigator.of(context).pop();
+                }
+              },
+              child: Text('Guardar'),
+            ),
+          ],
+        );
+      },
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,21 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:moonpv/point_sale/point_sale_newpage.dart';
-//import 'package:blue_thermal_printer/blue_thermal_printer.dart';
+import 'firebase_options.dart'; // Asegúrate de importar esto
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Cargar las variables de entorno antes de Firebase
+  await dotenv.load();
+
   try {
-    await Firebase.initializeApp();
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions
+          .currentPlatform, // Usando las opciones configuradas
+    );
   } catch (e) {
     print("Error initializing Firebase: $e");
   }
@@ -30,58 +38,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-class BluetoothInfo {
-  late String name;
-  late String macAddress; // Cambié 'macAdress' a 'macAddress'
-  //late BluetoothDevice device; Agrega una referencia al dispositivo Bluetooth
-}
-
-// Función para obtener los dispositivos Bluetooth emparejados
-// Future<List<BluetoothInfo>> getPairedBluetooths() async {
-//   BlueThermalPrinter printer = BlueThermalPrinter.instance;
-//   List<BluetoothDevice> devices = await printer.getBondedDevices();
-
-//   return devices.map((device) {
-//     return BluetoothInfo()
-//       ..name =
-//           device.name ?? 'Desconocido' // Proporciona un valor predeterminado
-//       ..macAddress =
-//           device.address ?? 'Desconocido' // Proporciona un valor predeterminado
-//       ..device = device; // Guarda el dispositivo Bluetooth
-//   }).toList();
-// }
-
-// Función para conectar a la impresora
-// Future<void> connectPrinter(BluetoothDevice device) async {
-//   BlueThermalPrinter printer = BlueThermalPrinter.instance;
-//   await printer.connect(device);
-//   print("Impresora conectada: ${device.name}");
-// }
-
-// Función para enviar datos de impresión a la impresora conectada
-// Future<void> printText(String text) async {
-//   BlueThermalPrinter printer = BlueThermalPrinter.instance;
-//   List<BluetoothDevice> devices = await printer.getBondedDevices();
-
-//   if (devices.isNotEmpty) {
-//     await printer.connect(devices[0]); // Conectar al primer dispositivo
-//     await printer.printCustom(text, 1, 1); // Imprimir texto
-//     await printer.disconnect(); // Desconectar después de imprimir
-//   } else {
-//     print("No hay dispositivos Bluetooth disponibles.");
-//   }
-// }
-
-// Ejemplo de uso en alguna parte de tu código para conectar e imprimir
-// Future<void> exampleUsage() async {
-//   List<BluetoothInfo> devices = await getPairedBluetooths();
-//   if (devices.isNotEmpty) {
-//     BluetoothDevice selectedDevice =
-//         devices[0].device; // Obtén el dispositivo Bluetooth
-//     await connectPrinter(selectedDevice);
-//     await printText("¡Hola, Mundo!"); // Enviar texto para imprimir
-//   } else {
-//     print("No hay impresoras emparejadas.");
-//   }
-// }
